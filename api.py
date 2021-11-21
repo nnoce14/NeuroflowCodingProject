46fb35903bbe1a929d90709f765e81348ef50a75
nnoce14 <nnoce12@gmail.com>
2021-11-20 22:38:37 -0500

    Added User model and allows POST to create new user login information at /api/users endpoint

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
-import os, time, jwt
-from flask import Flask, jsonify, g
-from flask_restful import Api
+import os, time
+from flask import Flask, jsonify, g, abort, request, url_for
+from flask_restful import Api, Resource
 from flask_httpauth import HTTPBasicAuth
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -9,6 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my super secret key is super long and secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 db = SQLAlchemy(app)
 auth = HTTPBasicAuth()
@@ -17,7 +18,7 @@ moods = []
 
 # Models
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(128))
@@ -38,9 +39,33 @@ def verify_password(username, password):
     return True
 
 # Endpoints
-@app.route('/api/mood', methods=['GET'])
+@app.route('/api/mood')
 def get_moods():
     return jsonify({'moods': moods})
 
+@app.route('/api/users', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return (jsonify({'username':user.username}), 201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
 if __name__ == "__main__":
+    if not os.path.exists('db.sqlite'):
+        db.create_all()
     app.run(debug=True)
