687f9505949e544d5d43b35ac0170c9570fa36a6
nnoce14 <nnoce12@gmail.com>
2021-11-20 19:46:54 -0500

    Added User model to api.py using SQLAlchemy. Added some dependencies so reran pip freeze on requirements.txt

--- api.py
+++ api.py
@@ -1,8 +1,39 @@
-from flask import Flask, jsonify
+import os, time, jwt
+from flask import Flask, jsonify, g
 from flask_restful import Api
+from flask_httpauth import HTTPBasicAuth
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Configuration
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my super secret key is super long and secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+db = SQLAlchemy(app)
+auth = HTTPBasicAuth()
+
+# Models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.String(128))
+
+    def hash_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# HTTPBasicAuth Security
+@auth.verify_password
+def verify_password(username, password):
+    user = User.query.filter_by(username=username).first()
+    if not user or not user.verify_password(password):
+        return False
+    g.user = user
+    return True
 
 # Endpoints
 @app.route('/api', methods=['GET'])
